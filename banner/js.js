35942fa93f2e9e6f54d4e93f538a04b7553f63dd
aligh <aghasizadeh70@gmail.com>
2012-04-13 12:33:13 +0430

    change some parts

--- banner/js.js
+++ banner/js.js
@@ -1,3 +1,13 @@
 window.onload = function(){
+	var height = 500;
+	var divScl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
+	var btn = document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
 	
+	for (var i=0;i<btn.length;i++){
+		(function(i){
+			btn.item(i).onclick = function(){
+				divScl.style.top=(-i*height) + 'px';
+			}
+		})(i);		
+	}
 }
