cb0fe32512dd1ef343856bebb1af42dac6b68dc3
aligh <aghasizadeh70@gmail.com>
2012-04-14 14:59:06 +0430

    the final changes :D

--- banner/js.js
+++ banner/js.js
@@ -1,13 +1,15 @@
 window.onload = function(){
-	var height = 500;
+	var width = 980;
+	var height = 670;
 	var divScl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
 	var txt = document.getElementsByClassName('banner').item(0).getElementsByClassName('text').item(0);
 	var btn = document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');	
 	for (var i=0;i<btn.length;i++){
 		(function(i){
 			btn.item(i).onclick = function(){
-				divScl.style.top=(-i*height) + 'px';
+				divScl.style.left=(-i*width) + 'px';
 				txt.style.top=(-i*height) + 'px';
+				//btn.item(i).style.backgroundColor='#008484';
 			}
 		})(i);		
 	}
