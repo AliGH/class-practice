e64562d7294548d1925030d6d6f9cc7d116975e2
aligh <aghasizadeh70@gmail.com>
2012-04-14 11:54:04 +0430

    fix the bug. add image

--- banner/js.js
+++ banner/js.js
@@ -1,13 +1,16 @@
 window.onload = function(){
 	var height = 500;
 	var divScl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
-	var btn = document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
-	
+	var txt = document.getElementsByClassName('banner').item(0).getElementsByClassName('text').item(0);
+	var btn = document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');	
 	for (var i=0;i<btn.length;i++){
 		(function(i){
 			btn.item(i).onclick = function(){
 				divScl.style.top=(-i*height) + 'px';
+				txt.style.top=(-i*height) + 'px';
 			}
 		})(i);		
 	}
+	
+	
 }
